5666f93c5ec92593fe821c1f8451d63801fce0fe
huhui123 <527273199@qq.com>
2018-01-23 19:18:02 +0800

    install antd

---
+++ web-react-project/src/pages/App.js
@@ -0,0 +1,17 @@
+import React, {
+  Component
+} from 'react';
+import {
+  Button
+} from 'antd';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        111111
+        <Button type="primary">Primary</Button>
+      </div>
+    );
+  }
+}
